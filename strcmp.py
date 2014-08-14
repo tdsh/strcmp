7dc30ee8be60e481d909e622549f9d43eb3a16a5
Tadashi Abe <tadashi.abe@gmail.com>
2014-08-14 15:38:24 +0900

    Prepare for Python 3
    Use __future__.

--- strcmp.py
+++ strcmp.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import sys
 
 if len(sys.argv) != 3:
-    print "Specify 2 strings which you want to compare."
+    print("Specify 2 strings which you want to compare.")
     sys.exit(1)
 
-print sys.argv[1] == sys.argv[2]
+print(sys.argv[1] == sys.argv[2])
